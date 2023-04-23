8dae45033ed0b63f87f9faae520224e608ea1c04
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-24 00:56:04 +0900

    add react loading but not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = () => {
+      // 非同期処理の記述
+      console.log('sample');
+      setLoading(false);
+    }
+  }, []);
+
   return (
-    <div>App</div>
+    <>
+      <div className="App">
+        {loading ? (
+          <p className='loader'>ロード中。。。</p>
+        ) : (
+          <>
+            <div className="">
+              ここに表示したい内容
+            </div>
+          </>
+        )}
+      </div>
+    </>
   )
 }
 
