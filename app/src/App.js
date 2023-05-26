b2788e7cb686cc1f89271ee4fe86081053ace94b
Kohta9521 <kohta0421kochi@gmail.com>
2023-05-26 15:01:50 +0900

    add next components fies

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Toppage from "./components/toppage/Top";
 
 function App() {
   return (
     <div className="App">
       <h1>sample code</h1>
+      <Toppage />
     </div>
   );
 }
